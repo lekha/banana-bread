4f3c1ce2f7d0be702bc6d2a899920f82dd8ac3fe
lekha <305097+lekha@users.noreply.github.com>
2019-01-27 11:38:36 -0500

    Allow users to select foods to judge

--- client/src/components/SelectFoods.vue
+++ client/src/components/SelectFoods.vue
@@ -2,11 +2,15 @@
     <div class='main'>
         <h1>Which banana foods have you sampled?</h1>
         <div class='icon-list'>
-            <div class='icon-wrapper' v-for='(food, index) in foods' :key='index'>
+            <div class='icon-wrapper' v-for='(food, index) in foods' :key='index' v-on:click='selectFood(index)'>
                 <img class='icon' v-bind:src='food.image_url'>
+                <transition name='fade'>
+                    <div class='overlay' v-if='food.selected'><i class='fas fa-check fa-5x'></i></div>
+                </transition>
                 <p>{{ food.owner }}'s</p>
             </div>
         </div>
+        <button>Start Judging</button>
     </div>
 </template>
 
@@ -18,17 +22,25 @@ export default {
             foods: [
                 {
                     'owner': 'Lekha',
-                    'image_url': 'https://www.simplyrecipes.com/wp-content/uploads/2014/08/banana-bread-vertical-c-1200.jpg'
+                    'image_url': 'https://www.simplyrecipes.com/wp-content/uploads/2014/08/banana-bread-vertical-c-1200.jpg',
+                    'selected': false
                 },
                 {
                     'owner': 'Mike K',
-                    'image_url': 'https://images-gmi-pmc.edge-generalmills.com/c23f59e1-1a55-4ba7-91de-71ca24e197fe.jpg'
+                    'image_url': 'https://images-gmi-pmc.edge-generalmills.com/c23f59e1-1a55-4ba7-91de-71ca24e197fe.jpg',
+                    'selected': false
                 },
                 {
                     'owner': 'Danielle',
-                    'image_url': 'https://www.landolakes.com/RecipeManagementSystem/media/Recipe-Media-Files/Recipes/Retail/x17/20643-walnut-banana-bread-600x600.jpg?ext=.jpg'
+                    'image_url': 'https://www.landolakes.com/RecipeManagementSystem/media/Recipe-Media-Files/Recipes/Retail/x17/20643-walnut-banana-bread-600x600.jpg?ext=.jpg',
+                    'selected': false
                 }
             ]
+        }
+    },
+    methods: {
+        selectFood(index) {
+            this.foods[index].selected = !this.foods[index].selected;
         }
     }
 }
@@ -51,6 +63,7 @@ h1 {
 .icon-wrapper {
     display: inline-block;
     height: 260px;
+    position: relative;
 }
 
 .icon {
@@ -60,5 +73,33 @@ h1 {
     border: 15px solid;
     border-radius: 10px;
     margin: 15px -7.5px -15px -7.5px;
+}
+
+.icon:hover {
+    filter: brightness(120%);
+}
+
+.overlay {
+    position: absolute;
+    top: 15px;
+    left: -7.5px;
+    height: 230px;
+    width: 230px;
+    display: grid;
+    align-items: center;
+    color: #50CB78;
+    background-color: rgba(127, 127, 127, 0.5);
+    background-clip: padding-box;
+    transition: 2s ease;
+    border: 15px solid rgba(0, 0, 0, 0);
+    border-radius: 10px;
+}
+
+.fade-enter-active, .fade-leave-active {
+    transition: opacity 0.3s;
+}
+
+.fade-enter, .fade-leave-to {
+    opacity: 0;
 }
 </style>
