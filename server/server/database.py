7ea249bf90c3f12d01f59005cc4f62d1c65dba3d
lekha <305097+lekha@users.noreply.github.com>
2019-01-27 22:57:27 -0500

    Clean up database call for food

--- server/server/database.py
+++ server/server/database.py
@@ -8,18 +8,14 @@ def connection():
 
 def fetch_foods():
     conn = connection()
-    cursor = conn.cursor()
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
     query = '''
-        SELECT food.id, baker.name, food.image_url
+        SELECT food.id, baker.name as owner, food.image_url
           FROM cafe.foods food
      LEFT JOIN cafe.bakers baker
             ON food.baker_id = baker.id
     '''
     cursor.execute(query)
-    foods = []
-    columns = ['id', 'owner', 'image_url']
-    for row in cursor:
-        foods.append(dict(zip(columns, row)))
+    foods = list(cursor.fetchall())
     conn.close()
-    print(foods)
     return foods
